62d82119b534cf0d13e3cf69b694e4cc136a4621
Tamás Kiss <bognor@saturn.rmki.kfki.hu>
2022-06-24 16:58:02 +0200

    Amount of communication
    Added collecting, calculation and plotting a measure quantifying the
    amount of task passing. Only a small modification in
    Scripts/ATRAN_SimpleComm.m was needed.
    
    As a plotting helper function, Functions/panel.m was added.

--- Scripts/ATRAN_SimpleComm.m
+++ Scripts/ATRAN_SimpleComm.m
@@ -37,8 +37,8 @@ par.nogvals = 40; %Number of group diversity steps
 
 % Internal simulation control parameters
 par.maxPass = Inf; %Each task can be passed this many times
-par.similThresh = Inf; %The threshold of similarity (as percentage of maximal distance) below which agents are willing to communicate w/ each other
-par.passCost = 0; % Steps, the cost of communication: the receiving agent cannot work until this amount of time stamps
+par.similThresh = 80; %The threshold of similarity (as percentage of maximal distance) below which agents are willing to communicate w/ each other
+par.passCost = 100; % Steps, the cost of communication: the receiving agent cannot work until this amount of time stamps
 
 % External simulation control parameters
 par.numrepeats = 10;
@@ -97,6 +97,7 @@ for ai = 1:length(adivvals)
         taskhists = NaN(par.numtasks, par.numfuncs, par.EmergencyStop+1, par.numrepeats); %Initialize variable keeping track of how tasks progress
         sn = NaN(par.numrepeats, 1); %Initialize number of steps nedded to solve all tasks
         stn = zeros(par.numrepeats, 1); %Initialize variable storing the amount of work done in repeats
+        ca = NaN(par.numrepeats, 1); %Init variable for communication amount
 
         %The following piece of code runs in parallel on multiple cores. Remove "par" to allow debugging of the inside.
         for ridx = 1:par.numrepeats
@@ -121,6 +122,11 @@ for ai = 1:length(adivvals)
             tmp1(isnan(tmp1)) = 0; %There can be NaNs initially due to sub-tasks that the system cannot solve!
             tmp2(isnan(tmp2)) = 0;
             stn(ridx) = sum(sum(tmp1 - tmp2));
+
+            % Calculate amount of communication
+            tmp = diff(t2a(:, :, ridx));
+            ca(ridx) = sum(sum(tmp ~= 0 & not(isnan(tmp))));
+
         end
         
         % Average steps
@@ -134,6 +140,12 @@ for ai = 1:length(adivvals)
         maxstn(ai, gi) = max(stn);
         meanstn(ai, gi) = mean(stn);
         stdstn(ai, gi) = std(stn);
+
+        % Average communication amount
+        minca(ai, gi) = min(ca);
+        maxca(ai, gi) = max(ca);
+        meanca(ai, gi) = mean(ca);
+        stdca(ai, gi) = std(ca);
 
         fprintf('.')
     end
@@ -151,12 +163,14 @@ clear adiv agents ai gdiv gi sn stn T t2a T_old taskhists
 %% Coarse-grain (average in bins) results and normalize max of amount of solved subtasks to 1
 cgmstn = NaN(length(par.IFDbins)-1, length(par.DFDbins)-1);
 cgmsn = NaN(size(cgmstn));
+cgca = NaN(size(cgmstn));
 cgIFD = NaN(size(cgmstn));
 cgDFD = NaN(size(cgmstn));
 for iidx = 1:length(par.IFDbins)-1
     for didx = 1:length(par.DFDbins)-1
         cgmstn(iidx, didx) = mean(meanstn(par.IFDbins(iidx)<=IFD & IFD<par.IFDbins(iidx+1) & par.DFDbins(didx)<=DFD & DFD<par.DFDbins(didx+1)));
         cgmsn(iidx, didx)  = mean(meansn(par.IFDbins(iidx)<=IFD & IFD<par.IFDbins(iidx+1) & par.DFDbins(didx)<=DFD & DFD<par.DFDbins(didx+1)));
+        cgca(iidx, didx)   = mean(meanca(par.IFDbins(iidx)<=IFD & IFD<par.IFDbins(iidx+1) & par.DFDbins(didx)<=DFD & DFD<par.DFDbins(didx+1)));
         cgIFD(iidx, didx)  = par.IFDbins(iidx);
         cgDFD(iidx, didx)  = par.DFDbins(didx);
     end
@@ -167,20 +181,44 @@ cgmstn = cgmstn/(par.numtasks*par.tnorm);
 % Interim cleanup
 clear iidx didx
 
-%% Plot amount of work solved vs. IFD, DFD (coarse-grained)
+%% Plot amount of work solved vs. IFD, DFD (coarse-grained), also amount of communication
 figure
+p = panel();
+p.pack(1,2)
+
+% The work plot
+p(1,1).select()
+ax1 = gca;
 surf(cgDFD, cgIFD, cgmstn)
+grid on
 title(['Passing cost=' num2str(par.passCost) ' | SimThresh=' num2str(par.similThresh) '% | NoAgs= ' ...
     num2str(par.numagents) ' | NoTasks= ' num2str(par.numtasks) ' | NoRepeats= ' num2str(par.numrepeats)])
 xlabel('Dominant Functional Diversity')
 ylabel('Individual Functional Diversity')
 zlabel('Average Solved Subtasks')
-cbh = colorbar;
-ylabel(cbh, 'Average Solved Subtasks')
+% cbh = colorbar;
+% ylabel(cbh, 'Average Solved Subtasks')
 xlim([0 1])
 ylim([0 1])
 zlim([0 1])
 clim([0 1])
+
+% The communication plot
+p(1,2).select()
+ax2 = gca;
+surf(cgDFD, cgIFD, cgca)
+grid on
+xlabel('Dominant Functional Diversity')
+ylabel('Individual Functional Diversity')
+zlabel('Amount of Communication')
+% cbh = colorbar;
+% ylabel(cbh, 'Amount of Communication')
+xlim([0 1])
+ylim([0 1])
+
+%Linking the two plots
+Link = linkprop([ax1, ax2],{'View'});
+setappdata(gcf, 'StoreTheLink', Link);
 
 %% Cuts
 
